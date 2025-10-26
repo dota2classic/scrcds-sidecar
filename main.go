9895e897be0e5a39bc195e0f197fb15b41a3ff3f
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-26 17:42:40 +0300

    Endpoints

--- main.go
+++ main.go
@@ -12,24 +12,23 @@ import (
 func main() {
 	go srcds.RunHeartbeatPoller()
 
-	http.HandleFunc("/live_match", handleJSONPost(func(data models.LiveMatchDto, w http.ResponseWriter) {
-		log.Printf("Received live_match: %+v", data)
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Hello %d, duration %d", data.MatchID, data.Duration)
-	}))
+	http.HandleFunc("/live_match", handleJSONPost(handleLiveMatch))
 
-	http.HandleFunc("/failed_match", handleJSONPost(func(data models.MatchFailedOnSRCDS, w http.ResponseWriter) {
-		log.Printf("Received failed_match: %+v", data)
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Match %d failed", data.MatchID)
-	}))
+	http.HandleFunc("/failed_match", handleJSONPost(handleMatchFailed))
+
+	http.HandleFunc("/player_not_loaded", handleJSONPost(handlePlayerNotLoaded))
+
+	http.HandleFunc("/player_abandon", handleJSONPost(handlePlayerAbandon))
+
+	http.HandleFunc("/player_connect", handleJSONPost(handlePlayerConnect))
+
+	http.HandleFunc("/match_results", handleJSONPost(handleMatchFinished))
 
 	port := 7777
 	log.Printf("Starting server on port %d...\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil))
 }
 
-// generic JSON handler wrapper
 func handleJSONPost[T any](handler func(T, http.ResponseWriter)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
@@ -45,4 +44,34 @@ func handleJSONPost[T any](handler func(T, http.ResponseWriter)) http.HandlerFun
 
 		handler(data, w)
 	}
+}
+
+func handleLiveMatch(data models.LiveMatchDto, w http.ResponseWriter) {
+	log.Printf("Received live_match: %+v", data)
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleMatchFailed(data models.MatchFailedOnSRCDS, w http.ResponseWriter) {
+	log.Printf("Received failed_match: %+v", data)
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlePlayerNotLoaded(data models.PlayerNotLoadedOnSRCDS, w http.ResponseWriter) {
+	log.Printf("Received player_not_loaded: %+v", data)
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlePlayerAbandon(data models.PlayerAbandonOnSRCDS, w http.ResponseWriter) {
+	log.Printf("Received player_abandon: %+v", data)
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlePlayerConnect(data models.PlayerConnectedOnSRCDS, w http.ResponseWriter) {
+	log.Printf("Received player_connected: %+v", data)
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleMatchFinished(data models.MatchFinishedOnSRCDS, w http.ResponseWriter) {
+	log.Printf("Received player_connected: %+v", data)
+	w.WriteHeader(http.StatusOK)
 }
