3356b05beab55c1bc396a3c1d8bcf501f3361ec2
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-27 16:33:31 +0300

    More logging

--- internal/rabbit/rabbit_client.go
+++ internal/rabbit/rabbit_client.go
@@ -7,6 +7,7 @@ import (
 
 func PublishMatchFailedEvent(event models.MatchFailedEvent) {
 	err := publishWithRetry(event, 3)
+	log.Println("Published MatchFailedEvent to rmq")
 	if err != nil {
 		log.Println("Error publishing event event:", err)
 		return
